655bec8c998d6c65c13be59c2ec3788f6181ed44
lukedoma1 <117953218+lukedoma1@users.noreply.github.com>
2024-12-06 13:48:47 -0600

    updated rotation speed

--- Final Project - Bee Balloon/Assets/__RedZoneScripts/LevelThree/Level3Zone1.cs
+++ Final Project - Bee Balloon/Assets/__RedZoneScripts/LevelThree/Level3Zone1.cs
@@ -7,7 +7,7 @@ public class Level3Zone1 : MonoBehaviour
     //public Vector3 rotationCenter = new Vector3(0.31f, 2.07f, -1.15f); // The point around which to rotate
     public Vector3 rotationCenter; // The point around which to rotate
     private Vector3 rotationAxis = Vector3.forward; // Axis of rotation (e.g., Vector3.up for Y-axis)
-    private float rotationSpeed = 40f; // Speed of rotation in degrees per second
+    private float rotationSpeed = 20f; // Speed of rotation in degrees per second
 
     public void SetCenter (Vector3 center) {
         rotationCenter = center;
