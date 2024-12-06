018ad9c5e730e2e96a84961d0b7ba5bb54f0d3a5
kene_M <maduabumkene@gmail.com>
2024-12-06 12:42:26 -0600

    Pause Menu Additions

--- Final Project - Bee Balloon/Assets/__Scripts/PauseMenu.cs
+++ Final Project - Bee Balloon/Assets/__Scripts/PauseMenu.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject PausePanel;
     public static bool isPaused = false;
 
+
     public void Pause()
     {
+        //Main.S.spawnButton.enabled? ;
+        //targetPoint = (targetPoint == pointA) ? pointB : pointA;
+        //Main.S.spawnButton.enabled = (Main.S.spawnButton.enabled)? false : false;
+
         PausePanel.SetActive(true);
         Time.timeScale = 0;
         isPaused = true;
@@ -19,5 +25,15 @@ public class PauseMenu : MonoBehaviour
         PausePanel.SetActive(false);
         Time.timeScale = 1;
         isPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+       Application.Quit();
     }
 }
